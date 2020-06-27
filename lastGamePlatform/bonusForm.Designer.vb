c598220f320e0ff46719055a7672a73bfe44b29c
Arouven <aroupoolian@gmail.com>
2020-06-27 12:06:33 +0400

    bonus scene added (trademark)

---
+++ lastGamePlatform/bonusForm.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class bonusForm
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(bonusForm))
+		Me.MainMenu = New System.Windows.Forms.Button()
+		Me.BonusScenePlayer = New AxWMPLib.AxWindowsMediaPlayer()
+		CType(Me.BonusScenePlayer, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'MainMenu
+		'
+		Me.MainMenu.Location = New System.Drawing.Point(543, 362)
+		Me.MainMenu.Name = "MainMenu"
+		Me.MainMenu.Size = New System.Drawing.Size(75, 23)
+		Me.MainMenu.TabIndex = 4
+		Me.MainMenu.Text = "Main Menu"
+		Me.MainMenu.UseVisualStyleBackColor = True
+		'
+		'BonusScenePlayer
+		'
+		Me.BonusScenePlayer.Enabled = True
+		Me.BonusScenePlayer.Location = New System.Drawing.Point(12, 12)
+		Me.BonusScenePlayer.Name = "BonusScenePlayer"
+		Me.BonusScenePlayer.OcxState = CType(resources.GetObject("BonusScenePlayer.OcxState"), System.Windows.Forms.AxHost.State)
+		Me.BonusScenePlayer.Size = New System.Drawing.Size(627, 398)
+		Me.BonusScenePlayer.TabIndex = 5
+		'
+		'bonusForm
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(659, 422)
+		Me.Controls.Add(Me.BonusScenePlayer)
+		Me.Controls.Add(Me.MainMenu)
+		Me.Name = "bonusForm"
+		Me.Text = "bonusForm"
+		CType(Me.BonusScenePlayer, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+
+	End Sub
+
+	Friend WithEvents MainMenu As Button
+	Friend WithEvents BonusScenePlayer As AxWMPLib.AxWindowsMediaPlayer
+End Class
--- lastGamePlatform/startHere.Designer.vb
+++ lastGamePlatform/startHere.Designer.vb
@@ -22,97 +22,110 @@ Partial Class startHere
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
-        Me.SuspendLayout()
-        '
-        'Button1
-        '
-        Me.Button1.Font = New System.Drawing.Font("Agency FB", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(146, 140)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(126, 36)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "1 Player"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Agency FB", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(138, 65)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(189, 45)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "COVID20 Game"
-        '
-        'Button2
-        '
-        Me.Button2.Font = New System.Drawing.Font("Agency FB", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.Location = New System.Drawing.Point(146, 228)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(126, 36)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "MultiPlayer"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'Button3
-        '
-        Me.Button3.Font = New System.Drawing.Font("Agency FB", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button3.Location = New System.Drawing.Point(146, 320)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(126, 36)
-        Me.Button3.TabIndex = 3
-        Me.Button3.Text = "Exit"
-        Me.Button3.UseVisualStyleBackColor = True
-        '
-        'Button4
-        '
-        Me.Button4.Font = New System.Drawing.Font("Agency FB", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button4.Location = New System.Drawing.Point(179, 270)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(64, 32)
-        Me.Button4.TabIndex = 4
-        Me.Button4.Text = "Score"
-        Me.Button4.UseVisualStyleBackColor = True
-        '
-        'Button5
-        '
-        Me.Button5.Font = New System.Drawing.Font("Agency FB", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button5.Location = New System.Drawing.Point(179, 177)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(64, 32)
-        Me.Button5.TabIndex = 5
-        Me.Button5.Text = "Score"
-        Me.Button5.UseVisualStyleBackColor = True
-        '
-        'startHere
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.DeepPink
-        Me.ClientSize = New System.Drawing.Size(452, 436)
-        Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button1)
-        Me.Name = "startHere"
-        Me.Text = "startHere"
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.Button3 = New System.Windows.Forms.Button()
+		Me.Button4 = New System.Windows.Forms.Button()
+		Me.Button5 = New System.Windows.Forms.Button()
+		Me.Button6 = New System.Windows.Forms.Button()
+		Me.SuspendLayout()
+		'
+		'Button1
+		'
+		Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button1.Location = New System.Drawing.Point(146, 140)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(126, 36)
+		Me.Button1.TabIndex = 0
+		Me.Button1.Text = "1 Player"
+		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.Location = New System.Drawing.Point(138, 65)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(288, 42)
+		Me.Label1.TabIndex = 1
+		Me.Label1.Text = "COVID20 Game"
+		'
+		'Button2
+		'
+		Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button2.Location = New System.Drawing.Point(146, 228)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(126, 36)
+		Me.Button2.TabIndex = 2
+		Me.Button2.Text = "MultiPlayer"
+		Me.Button2.UseVisualStyleBackColor = True
+		'
+		'Button3
+		'
+		Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button3.Location = New System.Drawing.Point(146, 320)
+		Me.Button3.Name = "Button3"
+		Me.Button3.Size = New System.Drawing.Size(126, 36)
+		Me.Button3.TabIndex = 3
+		Me.Button3.Text = "Exit"
+		Me.Button3.UseVisualStyleBackColor = True
+		'
+		'Button4
+		'
+		Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button4.Location = New System.Drawing.Point(179, 270)
+		Me.Button4.Name = "Button4"
+		Me.Button4.Size = New System.Drawing.Size(64, 32)
+		Me.Button4.TabIndex = 4
+		Me.Button4.Text = "Score"
+		Me.Button4.UseVisualStyleBackColor = True
+		'
+		'Button5
+		'
+		Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button5.Location = New System.Drawing.Point(179, 177)
+		Me.Button5.Name = "Button5"
+		Me.Button5.Size = New System.Drawing.Size(64, 32)
+		Me.Button5.TabIndex = 5
+		Me.Button5.Text = "Score"
+		Me.Button5.UseVisualStyleBackColor = True
+		'
+		'Button6
+		'
+		Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button6.Location = New System.Drawing.Point(146, 371)
+		Me.Button6.Name = "Button6"
+		Me.Button6.Size = New System.Drawing.Size(126, 36)
+		Me.Button6.TabIndex = 6
+		Me.Button6.Text = "Bonus"
+		Me.Button6.UseVisualStyleBackColor = True
+		'
+		'startHere
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.Color.DeepPink
+		Me.ClientSize = New System.Drawing.Size(452, 436)
+		Me.Controls.Add(Me.Button6)
+		Me.Controls.Add(Me.Button5)
+		Me.Controls.Add(Me.Button4)
+		Me.Controls.Add(Me.Button3)
+		Me.Controls.Add(Me.Button2)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.Button1)
+		Me.Name = "startHere"
+		Me.Text = "startHere"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
-    End Sub
+	End Sub
 
-    Friend WithEvents Button1 As Button
+	Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
     Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
 End Class
