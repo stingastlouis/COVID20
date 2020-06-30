fee99975ae56ba133c45f73323e74043078cbe9c
Pascal St Louis <stingastlouis@gmail.com>
2020-06-30 14:25:38 +0400

    moi sa

--- lastGamePlatform/PistoleBullet1.vb
+++ lastGamePlatform/PistoleBullet1.vb
@@ -2,8 +2,10 @@
 
     Inherits PictureBox
     Dim bmp As Bitmap = My.Resources._5a3719ae33d594_4783473215135604942123
-    Dim shootSpeed As Integer = 20
-    Public Sub New(ByVal ob As Object)
+	Dim shootSpeed As Integer = 20
+	Dim shootEnemy As Integer = 5
+	Dim shootBoss As Integer = 10
+	Public Sub New(ByVal ob As Object)
         With Me
             .Size = New Size(10, 30)
             .Location = New Point(ob.Width + ob.Left, ob.Top + 12)
@@ -27,12 +29,72 @@
 
         Me.Left += shootSpeed
 
-        'If Not b.left <= 500 Then
-        '    Me.Left -= shootSpeed
-        'End If
-    End Sub
+
+	End Sub
     Public Sub Shoot2(ByVal aa As Object)
 
         Me.Left -= shootSpeed
     End Sub
+
+	Private Sub bulletManager(ByRef Score As Integer, ByRef ProgressBar1 As ProgressBar, ByVal bullet1 As Array, ByVal player1 As Object, ByVal boss As Object, ByVal coins As Array, ByVal enemies As Array, ByVal lifes As Array, ByVal adns As Array)
+
+
+
+		For x As Integer = 0 To bullet1.Length - 1 '????????????????????????????????????????bizin recheck sa array la
+			bullet1(x).Shoot(player1)
+			If Module1.checkforCollision(bullet1(x), boss) And boss.Enabled = True And bullet1(x).Enabled = True Then
+				ProgressBar1.Value -= 1 '???????????????????????????????????????????????????????????????????p ggn exception kan boss mor
+				If ProgressBar1.Value <= 0 Then
+					ProgressBar1.Value = 0
+				End If
+				Score += shootBoss
+				updateLabels()
+				bullet1(x).Enabled = False
+				bullet1(x).Dispose()
+				boss.Enabled = False
+			End If
+
+			For Each coin As PictureBox In coins
+				If Module1.checkforCollision(bullet1(x), coin) And bullet1(x).Enabled And coin.Enabled Then
+					bullet1(x).Dispose()
+					Me.Controls.Remove(bullet1(x))
+					bullet1(x).Enabled = False
+				End If
+			Next
+			For Each enemy As PictureBox In enemies
+				If checkforCollision(bullet1(x), enemy) And bullet1(x).Enabled And enemy.Enabled Then
+					Score += shootEnemy
+					bullet1(x).Dispose()
+					bullet1(x).Enabled = False
+					'enemy.Enabled = False
+					Me.Controls.Remove(enemy)
+
+					ClassMyPublicShared.allPictureBoxes.Remove(enemy)
+					updateLabels()
+					Exit For ''----------------------------------------------------------------------------kifer exit----?mem zafr r break sa
+					'Me.Controls.Remove(enemy)
+				End If
+			Next
+
+
+			For Each life As PictureBox In lifes
+				If checkforCollision(bullet1(x), life) And bullet1(x).Enabled And life.Enabled Then
+					bullet1(x).Dispose()
+					Me.Controls.Remove(bullet1(x))
+					bullet1(x).Enabled = False
+				End If
+			Next
+
+			For Each adn As PictureBox In adns 'scan all controls present in form
+				If checkforCollision(bullet1(x), adn) And bullet1(x).Enabled And adn.Enabled Then
+					bullet1(x).Dispose()
+					Me.Controls.Remove(bullet1(x))
+					bullet1(x).Enabled = False
+				End If
+			Next
+			'-------------------------------------------
+		Next
+
+
+	End Sub
 End Class
