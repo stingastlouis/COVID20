ea1e0e28e70aaa53ad7e2c222f9ce97cf352ae00
Pascal St Louis <stingastlouis@gmail.com>
2020-07-09 18:34:52 +0400

    j

--- lastGamePlatform/startHere.vb
+++ lastGamePlatform/startHere.vb
@@ -6,7 +6,7 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Hide()
         Form1.Show()
-
+        'jkjp;
     End Sub
     'testing
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
